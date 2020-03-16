16267150083fabe712a2f309ed844ca0c518a642
Hayoung <hayoung43@nate.com>
2020-03-16 14:49:30 +0900

    반응형 디자인

--- src/components/Game/style.js
+++ src/components/Game/style.js
@@ -1,9 +1,17 @@
 import styled from "styled-components";
 
+const BREAK_POINT_SMALL = 600;
+const BREAK_POINT_MOBILE = 768;
+const BREAK_POINT_TABLET = 992;
+const BREAK_POINT_PC = 1200;
+
 export const FlexBox = styled.div`
   display: flex;
   flex-wrap: wrap;
   height: 100vh;
+  @media only screen and (max-width: ${BREAK_POINT_SMALL}px) {
+    flex-direction: column;
+  }
   .title {
     position: absolute;
     z-index: 2;
@@ -17,14 +25,19 @@ export const FlexBox = styled.div`
   }
   .flex-1 {
     flex: 1;
-    min-width: 500px;
     overflow: hidden;
     background-color: black;
     position: relative;
   }
+  .candidate-img {
     width: 100%;
     height: 100%;
-  .candidate-img {
+    position: absolute;
+    left: -100%;
+    right: -100%;
+    top: -100%;
+    bottom: -100%;
+    margin: auto;
     transition: 0.5s;
     cursor: pointer;
   }
@@ -37,7 +50,13 @@ export const FlexBox = styled.div`
     z-index: 3;
     color: #fff;
     bottom: 10%;
-    font-size: 90px;
+    @media only screen and (min-width: ${BREAK_POINT_TABLET}px) {
+      font-size: 90px;
+    }
+    @media only screen and (max-width: ${BREAK_POINT_TABLET}px) {
+      font-size: 36pt;
+    }
+    -webkit-text-stroke: 1px #000;
     left: 50%;
     transform: translateX(-50%);
   }
